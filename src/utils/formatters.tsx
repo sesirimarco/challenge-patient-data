745a72ead9013daae4a7bc20e1750f1648fde097
Sebastian Sirimarco <sesirimarco@gmail.com>
2023-12-11 20:57:50 -0300

    style: :green_heart: fix lint problems

--- src/utils/formatters.tsx
+++ src/utils/formatters.tsx
@@ -3,6 +3,6 @@ import { format } from "date-fns";
 export const formatDateTime = (dateTimeString: string) => {
 	const dateTime = new Date(dateTimeString);
 
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-	return format(dateTime, "MM/dd/yyyy HH:mm a ") as string;
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
+	return format(dateTime, "MM/dd/yyyy HH:mm a ");
 };
