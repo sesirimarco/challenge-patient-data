4792565c8b01c46a2aab6fdfcca6510a48f9807d
Sebastian Sirimarco <sesirimarco@gmail.com>
2023-12-11 12:53:01 -0300

    feat: :sparkles: add edit feature

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,24 +7,70 @@ import { formatDateTime } from "../../utils/formatters";
 
 interface CardProps {
 	patient?: Patient;
-
+	ellipsis?: boolean;
+	viewAction?: boolean;
+	editAction?: boolean;
 	onSelect: (data: Patient) => void;
+	onEdit?: (data: Patient) => void;
 }
 
-const Card: FC<CardProps> = ({ patient, onSelect }) => {
+const Card: FC<CardProps> = ({ patient, ellipsis, viewAction, editAction, onSelect, onEdit }) => {
 	return patient?.id.length ? (
-		<div key={patient.id} onClick={() => onSelect(patient)}>
+		<div onClick={() => onSelect(patient)}>
 			<div className="Card">
-				<img
-					src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/775.jpg"
-					alt="Avatar"
-					className="Card-image"
-				/>
+				<div className="Card-header">
+					{viewAction || editAction ? (
+						<div className="Card-header-actions">
+							{viewAction ? (
+								<div onClick={() => onSelect(patient)}>
+									<svg
+										fill="none"
+										color="white"
+										height="16"
+										stroke="currentColor"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										viewBox="0 0 24 24"
+										width="24"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+										<circle cx="12" cy="12" r="3" />
+									</svg>
+								</div>
+							) : null}
+							{editAction ? (
+								<div onClick={() => (onEdit ? onEdit(patient) : null)}>
+									<svg
+										fill="none"
+										height="14"
+										color="white"
+										stroke="currentColor"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										viewBox="0 0 24 24"
+										width="24"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+										<path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+									</svg>
+								</div>
+							) : null}
+						</div>
+					) : null}
+
+					<img src={patient.avatar} alt="Avatar" className="Card-header-image" />
+				</div>
 				<div className="Card-content">
 					<h2 className="Card-title">{patient.name}</h2>
 					<p className="Card-details">{formatDateTime(patient.createdAt)}</p>
-					<p className="Card-description Card-ellipsis-text">{patient.description}</p>
-					<a href={patient.website} className="Card-details">
+					<p className={`Card-description ${ellipsis ? "Card-ellipsis-text" : ""}`}>
+						{patient.description}
+					</p>
+					<a href={patient.website} className="Card-details" target="_blank" rel="noreferrer">
 						{patient.website}
 					</a>
 				</div>
--- src/routes/Patients.tsx
+++ src/routes/Patients.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 import Card from "../components/Card/Card";
 import Modal from "../components/Modal/Modal";
+import PatientForm from "../components/PatientForm/PatientForm";
 import { Patient } from "../interfaces/patients";
 import { useGetPatientsQuery } from "../store/services/patients";
 
@@ -11,24 +12,61 @@ const Patients = () => {
 	const { data, error, isLoading } = useGetPatientsQuery("");
 	const [showModal, setShowModal] = useState<boolean>(false);
 	const [selectedPatient, setSelectedPatient] = useState<Patient | undefined>(undefined);
+	const [edit, setEdit] = useState<boolean>(false);
 	console.log({ data });
 
 	return (
 		<>
-			<Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-				<Card onSelect={(data: Patient) => console.log(data)} patient={selectedPatient} />
+			<Modal
+				isOpen={showModal}
+				onClose={() => {
+					setEdit(false);
+					setShowModal(false);
+				}}
+			>
+				{edit ? (
+					<PatientForm
+						patient={selectedPatient}
+						onSave={(data: Patient) => console.log("save", data)}
+						onCancel={() => {
+							setEdit(false);
+							setShowModal(false);
+						}}
+					/>
+				) : (
+					<Card
+						onSelect={(data: Patient) => console.log(data)}
+						onEdit={(data: Patient) => {
+							setEdit(true);
+							setShowModal(true);
+							setSelectedPatient(data);
+						}}
+						patient={selectedPatient}
+						editAction
+					/>
+				)}
 			</Modal>
 			<h1>Patients</h1>
 			<div className="Patients-container">
 				{data?.length
 					? data.map((patient: Patient) => (
-							<Card
-								onSelect={(data: Patient) => {
-									setShowModal(true);
-									setSelectedPatient(data);
-								}}
-								patient={patient}
-							/>
+							<div key={patient.id}>
+								<Card
+									onSelect={(data: Patient) => {
+										setSelectedPatient(data);
+										setShowModal(true);
+									}}
+									onEdit={(data: Patient) => {
+										setEdit(true);
+										setShowModal(true);
+										setSelectedPatient(data);
+									}}
+									patient={patient}
+									ellipsis
+									viewAction
+									editAction
+								/>
+							</div>
 					  ))
 					: null}
 			</div>
